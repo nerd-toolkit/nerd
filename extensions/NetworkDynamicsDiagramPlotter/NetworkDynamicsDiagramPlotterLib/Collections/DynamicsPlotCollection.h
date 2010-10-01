576e6e37935be837b2cde707f1ea3cee0b8cdaaf
Christian Rempis <c.rempis@gmx.de>
2010-10-01 11:56:02 +0000

    Changed comment header of all project files in NetworkDynamicsDiagramPlotter.

--- extensions/NetworkDynamicsDiagramPlotter/NetworkDynamicsDiagramPlotterLib/Collections/DynamicsPlotCollection.h
+++ extensions/NetworkDynamicsDiagramPlotter/NetworkDynamicsDiagramPlotterLib/Collections/DynamicsPlotCollection.h
@@ -1,17 +1,24 @@
 /***************************************************************************
- *   NERD - Open Robot Control and Simulation Library                      *
+ *   NERD Kit - Neurodynamics and Evolutionary Robotics Development Kit    *
+ *                                                                         *
+ *   NetworkDynamicsPlotter project by Till Faber and Christian Rempis     *
+ *   till.faber@uni-osnabrueck.de
  *                                                                         *
  *   University of Osnabrueck, Germany                                     *
  *   Institute of Cognitive Science                                        *
  *   Neurocybernetics Group                                                *
  *   http://www.ikw.uni-osnabrueck.de/~neurokybernetik/                    *
  *                                                                         *
- *   Copyright (C) 2008 by Christian Rempis, Ferry Bachmann                *
- *   christian.rempis@uni-osnabrueck.de +                                  *
+ *   Project homepage: nerd.x-bot.org                                      *
+ *                                                                         *
+ *   Copyright (C) 2008 - 2010 by the Neurocybernetics Groups Osnabrück    *
+ *   Contact: Christian Rempis                                             *
+ *   christian.rempis@uni-osnabrueck.de                                    *
+ *   Contributors: see contributors.txt in the nerd main directory.        *
  *                                                                         *
  *                                                                         *
  *   Acknowledgments:                                                      *
- *   The NERD library is part of the EU project ALEAR                      *
+ *   The NERD Kit is part of the EU project ALEAR                          *
  *   (Artificial Language Evolution on Autonomous Robots) www.ALEAR.eu     *
  *   This work is funded by EU-Project Number ICT 214856                   *
  *                                                                         *
@@ -32,6 +39,9 @@
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   Publications based on work using the NERD kit have to state this      *
+ *   clearly by citing the nerd homepage and the nerd overview paper.      *
  ***************************************************************************/
 
 
