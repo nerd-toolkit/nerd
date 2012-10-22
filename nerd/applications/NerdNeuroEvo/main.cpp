226dc5c06d36913d1a80b6997bc9919a254d032c
Joseph Behr <jbehr@uni-osnabrueck.de>
2012-10-22 12:52:31 +0000

    XInitThreads call for NerdNeuroEvo

--- nerd/applications/NerdNeuroEvo/main.cpp
+++ nerd/applications/NerdNeuroEvo/main.cpp
@@ -55,6 +55,10 @@
 #include <Mmsystem.h>
 #endif
 
+#ifdef Q_WS_X11
+#include <X11/Xlib.h>
+#endif
+
 using namespace std;
 using namespace nerd;
 
@@ -62,6 +66,10 @@ int main(int argc, char *argv[])
 {
 #ifdef _WIN32
 	timeBeginPeriod(1);
+#endif
+
+#ifdef Q_WS_X11
+	XInitThreads();
 #endif
 
 	//initialize ressources (compiled images, etc.)
